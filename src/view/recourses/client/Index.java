0a7fc27eb3b1396967941d2f2a6edef9f535c991
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-19 23:35:28 +0100

    Update Index.java

--- src/view/recourses/client/Index.java
+++ src/view/recourses/client/Index.java
@@ -9,7 +9,7 @@ public class Index extends View {
 
     public void display() {
         System.out.println("\t\t-Client Index-\t<1>\n");
-        System.out.println("\t1. Create new clinet.");
+        System.out.println("\t1. Create new client.");
         System.out.println("\t2. Search existent client.");
         // router.get(1+(input.getNum("")/10));
         Router.get(1, input.getNum("Enter your choice")).display();
