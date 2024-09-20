00fb0a7aac2175ac97bdfdfa0d5a2cf43d6b6498
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-20 15:44:13 +0100

    update client search view

--- src/view/recourses/client/Search.java
+++ src/view/recourses/client/Search.java
@@ -1,14 +1,17 @@
 package view.recourses.client;
 
+import daoImpl.ClientDaoImpl;
+import model.Client;
+import service.ClientService;
 import utils.Input;
 import interfaces.View;
 
 public class Search extends View {
     private Input input = new Input();
+    private ClientService service = new ClientService();
 
     public void display(){
-        System.out.println("Cient Search");
-        input.getNum("");
-        input.getPhone("");
+        System.out.println("\t\t-Client Search-\n");
+        Client client = service.searchByName(input.getStr("Enter client name"));
     }
 }
