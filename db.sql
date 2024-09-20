0e11c3567383510ac52ba7a942e55dde20a7514a
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-20 15:42:36 +0100

    update db

--- db.sql
+++ db.sql
@@ -1,32 +1,47 @@
--- set enum columns
-CREATE TYPE status_enum AS ENUM ("IN_PROGRESS", "DONE", "CANCELED");
+-- set enum columns if not exists
+CREATE TYPE status_enum AS ENUM ('IN_PROGRESS', 'DONE', 'CANCELED');
 
 -- create db
 -- CREATE DATABASE bati_cuisine;
 
-CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    address TEXT,
-    phone VARCHAR(244),
-    is_professional BOOLEAN
+CREATE TABLE IF NOT EXISTS clients (
+    id SERIAL PRIMARY KEY UNIQUE,
+    name VARCHAR(70) UNIQUE NOT NULL,
+    address VARCHAR(250) NOT NULL,
+    phone VARCHAR(12) NOT NULL,
+    is_professional BOOLEAN DEFAULT FALSE
 );
 
 CREATE TABLE IF NOT EXISTS projects (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255),
-    benefit_margin NUMERIC,
-    total_cost NUMERIC,
-    status status_enum
+    name VARCHAR(70) UNIQUE NOT NULL,
+    benefit_margin NUMERIC(11, 4) NOT NULL,
+    total_cost NUMERIC(20, 4) NOT NULL,
+    status status_enum NOT NULL,
+    client_id
+);
+
+CREATE TABLE IF NOT EXISTS components (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(70) UNIQUE NOT NULL,
+    unit_cost NUMERIC(20, 4) NOT NULL,
+    quantity NUMERIC(14, 4) NOT NULL,
+    type VARCHAR(40) NOT NULL,
+    tva_rate NUMERIC NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS quotation (
-    id SERIAL PRIMARY KEY,
-    estimated_amount NUMERIC,
-    issue_date DATE,
-    valid_until DATE,
-    is_accepted BOOLEAN
-);
+    estimated_amount NUMERIC(14, 4) NOT NULL,
+    issue_date DATE NOT NULL,
+    valid_until DATE NOT NULL,
+    is_accepted BOOLEAN DEFAULT FALSE
+) INHERITS (components);
+
+CREATE TABLE IF NOT EXISTS labors (
+    hourly_rate NUMERIC(11, 4) NOT NULL,
+    hours_worked NUMERIC(11, 4) NOT NULL,
+    worker_productivity NUMERIC(6,4) NOT NULL
+) INHERITS (components);
 
 
 
