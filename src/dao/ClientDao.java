c65e3b05fa381db36c50ad9e3fd489c372d72e43
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-21 16:55:57 +0100

    change method return type

--- src/dao/ClientDao.java
+++ src/dao/ClientDao.java
@@ -6,7 +6,7 @@ import model.Client;
 public interface ClientDao {
     public HashMap<String, Client> getAll();
     public Client get(int id);
-    public void create(Client user);
+    public Integer create(Client user);
     public void update(Client client);
     public void delete(int id);
 
