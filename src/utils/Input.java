ba543f6f509e118bdb33efd0f5e6fd49ed097503
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-21 16:53:28 +0100

    add input getDouble method

--- src/utils/Input.java
+++ src/utils/Input.java
@@ -1,31 +1,41 @@
 package utils;
 
 
+import com.sun.org.apache.bcel.internal.generic.RETURN;
 import com.sun.org.slf4j.internal.Logger;
 import com.sun.org.slf4j.internal.LoggerFactory;
+import jdk.nashorn.internal.runtime.options.LoggingOption;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.InputMismatchException;
 import java.util.Scanner;
+import java.util.function.Supplier;
 
 
 public class Input {
     private Scanner scan = new Scanner(System.in);
     private Logger logger = LoggerFactory.getLogger(Input.class);
 
-    public int getNum(String message) {
+    private <T> T numTemplate(String message, Supplier<T> supplier){
         while (true) {
             try {
                 System.out.println(message != "" ? (">>> " + message + ":") : ">>> Enter number: ");
-                return scan.nextInt();
+                return supplier.get();
             } catch (InputMismatchException e) {
                 logger.error("[-] Please enter valid number.\n");
                 scan.nextLine();
             }
         }
     }
+    public int getNum(String message) {
+        return numTemplate(message, () -> scan.nextInt());
+    }
+    public double getDouble(String message) {
+        return numTemplate(message, () -> scan.nextDouble());
+    }
+
     public String getStr(String message){
         while (true) {
             System.out.println(message != "" ? (">>> " + message + ":") : ">>> Enter text:");
@@ -59,7 +69,7 @@ public class Input {
 
     public boolean getYesNo(String message){
         while (true) {
-            System.out.println(message != "" ? (">>> " + message + ":") : ">>> You want ? (y/n) : ");
+            System.out.println(message != "" ? (">>> " + message + " : ") : ">>> You want ? (y/n) : ");
             String input = scan.nextLine();
             if (input.toLowerCase().matches("y||n")) return input.equalsIgnoreCase("y");
             else logger.error("[-] Input must be either \"y/Y\" or \"n/N\"\n");
@@ -73,5 +83,4 @@ public class Input {
             if (input.replaceAll("[\\s\\-]", "").matches("\\+[0-9]{10,14}")) return input.replaceAll("[\\s\\-]", ""); else logger.error("[-] Input must be like +XXXXXXXXXX (10 to 14 numbers, note: can contain \" \" or \"-\").\n");
         }
     }
-
 }
