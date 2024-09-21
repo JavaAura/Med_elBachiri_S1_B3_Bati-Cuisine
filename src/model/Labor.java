f20a27ab7ef0c5d21bdf938a9de23166d884b8fb
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-21 17:01:53 +0100

    Update Labor.java

--- src/model/Labor.java
+++ src/model/Labor.java
@@ -5,13 +5,14 @@ public class Labor extends Component {
     private double hoursWorked;
     private double workerProductivity;
 
-    public Labor(String name, double unitCost, double quantity, String type, double TVARate, double hourlyRate, double hoursWorked, double workerProductivity) {
-        super(name, unitCost, quantity, type, TVARate);
+    public Labor(String name, String componentType, double TVARate, double hourlyRate, double hoursWorked, double workerProductivity) {
+        super(name, componentType, TVARate);
         this.hourlyRate = hourlyRate;
         this.hoursWorked = hoursWorked;
         this.workerProductivity = workerProductivity;
     }
 
+    // getters
     public double getHourlyRate() { return hourlyRate; }
     public double getHoursWorked() { return hoursWorked; }
     public double getWorkerProductivity() { return workerProductivity; }
