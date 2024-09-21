942d50c90cbb2db32c1863d5f35923312a1f6230
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-21 16:58:39 +0100

    Update client/Create.java

--- src/view/recourses/client/Create.java
+++ src/view/recourses/client/Create.java
@@ -1,21 +1,28 @@
 package view.recourses.client;
 
 import interfaces.View;
+import model.Client;
+import service.ClientService;
 import utils.Input;
 import view.routing.Router;
 
 public class Create extends View {
     private Input input = new Input();
-    public void display(){
-        System.out.println("\t\t-Create new client-\t<1.1>\n");
-        getClient();
-        System.out.println("[+] client added.");
-        boolean continueWclient = input.getYesNo("want to continue with this client");
-        if (continueWclient) Router.get("project_create").display(); else Router.get("client").display();
+
+    public void display(Object... params){
+        System.out.println("\t\t-Create new client-\n");
+        Integer cId = ClientService.create(getClient());
+        if(cId != null){
+            boolean keepThis = input.getYesNo("do you want to continue with this client?");
+            if (keepThis) Router.get("project_create").display(cId); else display(); // Router.get("client").display();
+        } else display();
     }
-    private void getClient(){
+
+    private Client getClient(){
         String name = input.getStr("Enter the name of the client");
         String address = input.getStr("Enter address of the client", true);
         String phone = input.getPhone("Enter phone number of the client (+2127000000)");
+        boolean isProfessional = input.getYesNo("Is this client professional?");
+        return new Client(name, address, phone, isProfessional);
     }
 }
