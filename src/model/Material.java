1524d1ca95b20bc050fd18b29df66fbd82ca2f2a
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-21 17:01:48 +0100

    Update Material.java

--- src/model/Material.java
+++ src/model/Material.java
@@ -1,15 +1,22 @@
 package model;
 
 public class Material extends Component {
+    private double unitCost;
+    private double quantity;
     private double transportCost;
     private double coefficientQuality;
 
-    public Material(String name, double unitCost, double quantity, String type, double TVARate, double transportCost, double coefficientQuality) {
-        super(name, unitCost, quantity, type, TVARate);
+    public Material(String name, String componentType, double TVARate, double unitCost, double quantity, double transportCost, double coefficientQuality) {
+        super(name, componentType, TVARate);
+        this.unitCost = unitCost;
+        this.quantity = quantity;
         this.transportCost = transportCost;
         this.coefficientQuality = coefficientQuality;
     }
 
+    // getters
+    public double getUnitCost() { return unitCost; }
+    public double getQuantity() { return quantity; }
     public double getTransportCost() { return transportCost; }
     public double getCoefficientQuality() { return coefficientQuality; }
 }
