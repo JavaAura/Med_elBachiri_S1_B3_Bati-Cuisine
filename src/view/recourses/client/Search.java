80bb821c64793a7e30ceeaee947b7840dd9e35e8
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-21 16:57:10 +0100

    handle if clients list is empty

--- src/view/recourses/client/Search.java
+++ src/view/recourses/client/Search.java
@@ -1,17 +1,23 @@
 package view.recourses.client;
 
-import daoImpl.ClientDaoImpl;
 import model.Client;
 import service.ClientService;
 import utils.Input;
 import interfaces.View;
+import view.routing.Router;
 
 public class Search extends View {
     private Input input = new Input();
     private ClientService service = new ClientService();
 
-    public void display(){
+    public void display(Object... params){
         System.out.println("\t\t-Client Search-\n");
         Client client = service.searchByName(input.getStr("Enter client name"));
+        if(client == null) System.out.println("\t[-] client not found.\n"); Router.get("clients").display();
+        assert client != null;
+        client.display();
+        if(input.getYesNo("do you want to continue with this client?"))
+            Router.get("project_create").display(client.getId());
+        else display();
     }
 }
