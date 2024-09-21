29c5587c8eba5c3caf11f813ee7c937e79fbac0a
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-21 16:53:36 +0100

    Update Quite.java

--- src/view/recourses/quite/Quite.java
+++ src/view/recourses/quite/Quite.java
@@ -3,7 +3,7 @@ package view.recourses.quite;
 import interfaces.View;
 
 public class Quite extends View {
-    public void display(){
+    public void display(Object... params){
         System.out.println("\n\n\t\tGoodbye ^^ !\n\n");
     }
 }
