169c30d9615fb6d990e6f48b590385145058d3ee
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-21 16:56:26 +0100

    Update Create.java

--- src/view/recourses/project/Create.java
+++ src/view/recourses/project/Create.java
@@ -6,13 +6,14 @@ import interfaces.View;
 public class Create extends View {
     private Input input = new Input();
 
-    public void display() {
+    public void display(Object ... params) {
+        int id = (Integer) params[0];
         System.out.println("\t\t-Create Project-\n");
 
     }
 
     private void getProject() {
-        String name = input.getStr("Enter project name", false);
-        int surface = input.getNum("Entrez la surface de la cuisine (en m²)");
+        String name = input.getStr("Enter project name");
+        double surface = input.getDouble("Enter the kitchen area (in m²)");
     }
 }
