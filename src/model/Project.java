7c879496f5e8496493c7cde012777a419fff40bb
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-21 17:01:43 +0100

    update Project model, add kitchen area attribute/get/set

--- src/model/Project.java
+++ src/model/Project.java
@@ -1,35 +1,35 @@
 package model;
 
-
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
-
 public class Project {
     private int id;
     private String name;
     private double benefitMargin;
     private double totalCost;
-    public enum Status{ IN_PROGRESS, DONE, CANCELED };
     private final Status status;
-    private static final Logger logger = LoggerFactory.getLogger(Project.class);
+    private double kitchenAreaM2;
 
-    public Project(String name, double benefitMargin, double totalCost, Status status){
+    public enum Status{ IN_PROGRESS, DONE, CANCELED };
+
+    public Project(String name, double kitchenAreaM2, double benefitMargin, double totalCost, Status status){
         this.name = name;
+        this.kitchenAreaM2 = kitchenAreaM2;
         this.benefitMargin = benefitMargin;
         this.totalCost = totalCost;
         this.status = status;
     }
 
-    public void setId(int id) { this.id = id; }
-    public int getId() { return id; }
 
+    // getters
+    public int getId() { return id; }
     public String getName() { return name; }
+    public double getKitchenAreaM2() { return kitchenAreaM2; }
     public double getBenefitMargin() { return benefitMargin; }
     public double getTotalCost() { return totalCost; }
 
+    // setters
+    public void setId(int id) { this.id = id; }
     public void setName(String name) { this.name = name; }
+    public void setKitchenAreaM2(double kitchenAreaM2) { this.kitchenAreaM2 = kitchenAreaM2; }
     public void setBenefitMargin(double benefitMargin) { this.benefitMargin = benefitMargin; }
     public void setTotalCost(double totalCost) { this.totalCost = totalCost; }
-    public Logger getLogger(){ return logger; }
-    public Status getStatus(){ return this.status; }
 }
