66e9becc4d55eefe2909984d2a02f8b056bfa860
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-21 16:54:01 +0100

    Update Show.java

--- src/view/recourses/project/Show.java
+++ src/view/recourses/project/Show.java
@@ -5,7 +5,7 @@ import interfaces.View;
 
 public class Show extends View{
     // private Router router = new Router();
-    public void display(){
+    public void display(Object... params){
         System.out.println("\t\t-Projects-\n");
         System.out.println("\tProject 1:");
         System.out.println("\n\tName: Bati-Cuisine");
