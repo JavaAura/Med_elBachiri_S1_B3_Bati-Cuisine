c77cad4f419be67c601fec352d7247c68813b36c
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-23 00:20:08 +0100

    add laborCost -> Labor & add display method

--- src/model/Labor.java
+++ src/model/Labor.java
@@ -4,9 +4,10 @@ public class Labor extends Component {
     private double hourlyRate;
     private double hoursWorked;
     private double workerProductivity;
+    private double laborCost;
 
-    public Labor(String name, String componentType, double TVARate, double hourlyRate, double hoursWorked, double workerProductivity) {
-        super(name, componentType, TVARate);
+    public Labor(String name, String componentType, double hourlyRate, double hoursWorked, double workerProductivity) {
+        super(name, componentType);
         this.hourlyRate = hourlyRate;
         this.hoursWorked = hoursWorked;
         this.workerProductivity = workerProductivity;
@@ -16,4 +17,13 @@ public class Labor extends Component {
     public double getHourlyRate() { return hourlyRate; }
     public double getHoursWorked() { return hoursWorked; }
     public double getWorkerProductivity() { return workerProductivity; }
+    public double getLaborCost() { return laborCost; }
+    // setters
+    public void setLaborCost(double laborCost) { this.laborCost = laborCost; }
+
+    public void display(){
+        System.out.println(
+                "- " + this.getName() + ": " + this.laborCost + " MAD (hourly rate: " + hourlyRate + " MAD/h, worked hours: " + hoursWorked + " h, productivity: " + workerProductivity  + ")"
+        );
+    }
 }
