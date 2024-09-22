2b338767cb0ddfa15b78a4dca77121d5cc42d941
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-23 00:19:27 +0100

    Update Component.java

--- src/model/Component.java
+++ src/model/Component.java
@@ -4,14 +4,20 @@ public class Component {
     private String name;
     private String componentType;
     private double TVARate;
+    private int projectId;
 
-    public Component(String name, String componentType, double TVARate){
+    public Component(String name, String componentType){
         this.name = name;
         this.componentType = componentType;
-        this.TVARate = TVARate;
     }
 
+    // getters
     public String getName() { return name; }
     public String getComponentType() { return componentType; }
     public double getTVARate() { return TVARate; }
+    public int getProjectId() { return projectId; }
+
+    // setters
+    public void setTVARate(double TVARate) { this.TVARate = TVARate; }
+    public void setProjectId(int projectId) { this.projectId = projectId; }
 }
