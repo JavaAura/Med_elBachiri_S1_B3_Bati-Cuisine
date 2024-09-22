265c0ee5f25e210b099e0b7f657ef628733162ea
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-23 00:24:05 +0100

    update client create view

--- src/view/recourses/client/Create.java
+++ src/view/recourses/client/Create.java
@@ -6,14 +6,16 @@ import service.ClientService;
 import utils.Input;
 import view.routing.Router;
 
-public class Create extends View {
+public class Create implements View {
     private Input input = new Input();
 
     public void display(Object... params){
         System.out.println("\t\t-Create new client-\n");
-        Integer cId = ClientService.create(getClient());
+        Client c = getClient();
+        Integer cId = ClientService.create(c);
         if(cId != null){
-            boolean keepThis = input.getYesNo("do you want to continue with this client?");
+            c.display();
+            boolean keepThis = input.getYesNo("do you want to continue with this client");
             if (keepThis) Router.get("project_create").display(cId); else display(); // Router.get("client").display();
         } else display();
     }
@@ -22,7 +24,7 @@ public class Create extends View {
         String name = input.getStr("Enter the name of the client");
         String address = input.getStr("Enter address of the client", true);
         String phone = input.getPhone("Enter phone number of the client (+2127000000)");
-        boolean isProfessional = input.getYesNo("Is this client professional?");
+        boolean isProfessional = input.getYesNo("Is this client professional");
         return new Client(name, address, phone, isProfessional);
     }
 }
