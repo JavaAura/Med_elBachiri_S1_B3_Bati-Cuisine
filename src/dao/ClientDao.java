58913a37747782420dcf84105b2f93cbbcee8ce2
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-23 19:47:00 +0100

    Update ClientDao.java

--- src/dao/ClientDao.java
+++ src/dao/ClientDao.java
@@ -7,7 +7,4 @@ public interface ClientDao {
     public HashMap<String, Client> getAll();
     public Client get(int id);
     public Integer create(Client client);
-    public void update(Client client);
-    public void delete(int id);
-
 }
