69453e45c97d1597a7e9ff46c94f05cf93936be6
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-23 19:52:48 +0100

    Update ProjectDaoImpl, get project with client, set tva, set benefit margin

--- src/daoImpl/ProjectDaoImpl.java
+++ src/daoImpl/ProjectDaoImpl.java
@@ -7,6 +7,7 @@ import model.Material;
 import model.Project;
 import utils.db.ModelCrud;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,7 +24,15 @@ public class ProjectDaoImpl implements ProjectDao {
 
     @Override
     public Project get(int id) {
-        return null;
+        HashMap<Integer, List<Object>> p = new HashMap<>();
+        model.activateWhere(true); model.where("id"); model.equalsTo(id);
+        p = model.getAll();
+        List<Object> obj = p.get(id);
+        Project project = new Project((String) obj.get(1), (double) obj.get(5));
+        project.setBenefitMargin((double) obj.get(2));
+        project.setTotalCost((double) obj.get(3));
+        project.setClientId((int) obj.get(6));
+        return project;
     }
 
     @Override
@@ -34,30 +43,88 @@ public class ProjectDaoImpl implements ProjectDao {
 
     @Override
     public List<Material> getMaterials(int projectId) {
+        model.setTable(ModelCrud.Table.MATERIALS);
         List<Material> materials = new ArrayList<>();
+        model.activateWhere(true);
         model.where("project_id");
         model.equalsTo(projectId);
         model.getAll().forEach((k, v) -> {
-            Material material = new Material((String) v.get(1), (String) v.get(2), (double) v.get(5), (double) v.get(6), (double) v.get(7), (double) v.get(8));
-            material.setMaterialCost((double) v.get(9));
-            material.setTVARate((double) v.get(3));
+            Material material = new Material(
+                    (String) v.get(1),
+                    (String) v.get(2),
+                    ((BigDecimal) v.get(5)).doubleValue(),
+                    ((BigDecimal) v.get(6)).doubleValue(),
+                    ((BigDecimal) v.get(7)).doubleValue(),
+                    ((BigDecimal) v.get(8)).doubleValue()
+            );
+            material.setMaterialCost(((BigDecimal) v.get(9)).doubleValue());
+            material.setTVARate(((BigDecimal) v.get(3)).doubleValue());
             material.setProjectId((int) v.get(0));
+            materials.add(material);
+
         });
         return materials;
     }
 
     @Override
     public List<Labor> getLabors(int projectId){
+        model.setTable(ModelCrud.Table.LABORS);
         List<Labor> labors = new ArrayList<>();
+        model.activateWhere(true);
         model.where("project_id");
         model.equalsTo(projectId);
         model.getAll().forEach((k, v) -> {
-            Labor labor = new Labor((String) v.get(1), (String) v.get(2), (double) v.get(5), (double) v.get(6), (double) v.get(7));
-            labor.setLaborCost((double) v.get(8));
-            labor.setTVARate((double) v.get(3));
+            Labor labor = new Labor(
+                    (String) v.get(1),
+                    (String) v.get(2),
+                    ((BigDecimal) v.get(5)).doubleValue(), // up to tables column order
+                    ((BigDecimal) v.get(6)).doubleValue(),
+                    ((BigDecimal) v.get(7)).doubleValue()
+            );
+            labor.setLaborCost(((BigDecimal) v.get(8)).doubleValue());
+            labor.setTVARate(((BigDecimal) v.get(3)).doubleValue());
             labor.setProjectId((int) v.get(0));
+            labors.add(labor);
         });
+
         return labors;
     }
+    @Override
+    public boolean setTva(int projectId, double tva){
+        model.setTable(ModelCrud.Table.COMPONENTS);
+        model.setColumns("tva_rate");
+        model.where("project_id"); model.equalsTo(projectId);
+        model.setValues(tva);
+        model.setThroughMsg(false);
+        return model.update();
+    }
 
+    @Override
+    public boolean setBenefitMargin(int projectId, double benefitMargin){
+        model.setTable(ModelCrud.Table.PROJECTS);
+        model.setColumns("benefit_margin");
+        model.where("id"); model.equalsTo(projectId);
+        model.setValues(benefitMargin);
+        model.setThroughMsg(false);
+        return model.update();
+    }
+
+    @Override
+    public HashMap<String, Object> projectNClient(int projectId){
+        HashMap<String, Object> pnc = new HashMap<>();
+        model.activateWhere(true); model.join("clients"); model.on("clients.id = projects.client_id");
+        model.where("projects.id"); model.equalsTo(projectId);
+        List<Object> c =  model.getAll().get(projectId);
+
+        pnc.put("project_name", c.get(1));
+        pnc.put("benefit_margin", c.get(2));
+        pnc.put("total_cost", c.get(3));
+        pnc.put("status", c.get(4));
+        pnc.put("kitchen_area", c.get(5));
+        pnc.put("name", c.get(8));
+        pnc.put("address", c.get(9));
+        pnc.put("phone", c.get(10));
+        pnc.put("is_professional", c.get(11));
+        return pnc;
+    }
 }
