de9caef950f042956d09a0b780db40942dc95d9f
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-23 19:51:25 +0100

    update project Details view

--- src/view/recourses/project/Details.java
+++ src/view/recourses/project/Details.java
@@ -1,24 +1,30 @@
 package view.recourses.project;
 
-// import views.Router;
-import interfaces.View;
-import model.Material;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
+import interfaces.View;
+import service.ClientService;
+import service.ProjectService;
+
 
 public class Details implements View{
-    // private Router router = new Router();
+    private ProjectService service = new ProjectService();
+
     public void display(Object... params){
-        Array materials = (Array) params[0]; // array of materials
-        Array labors = (Array) params[0]; // array of labors
+        int projectId = (int) params[0]; // project id
+
+        // get project and client, method return benefit margin
+        double benefitMargin = service.getProjectClient(projectId);
+
+        System.out.println("\n\t\t--- Cost Breakdown ---\n");
+        double materialsCostTVA = service.getMaterials(projectId);
+        double laborsCostTVA = service.getLabors(projectId);
+        double totalCostBeforeMargin = materialsCostTVA + laborsCostTVA;
+        double benefitMarginCost = totalCostBeforeMargin * (benefitMargin/100);
+
+        System.out.println("\n\t\t<3> Total cost before margin: \n\t\t" + totalCostBeforeMargin + "MAD");
+        System.out.println("\n\t\t<4> Profit margin ("+benefitMargin+"%): \n\t\t" + benefitMarginCost + "MAD");
+        System.out.println("\n\t\t**Final total cost of the project: \n\t\t" + (totalCostBeforeMargin + benefitMarginCost) + "MAD**");
 
 
-        System.out.println("\t\t-Projects-\n");
-        System.out.println("\tProject 1:");
-        System.out.println("\n\tName: Bati-Cuisine");
-        System.out.println("\tCient: Med Assami");
-        // router.get("main");
     }
 }
