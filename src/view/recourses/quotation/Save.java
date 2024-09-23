03eda0f56b43945b4ff2bdd0360bfb7f0efe0ddc
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-23 19:51:43 +0100

    add project save quotation primary view

---
+++ src/view/recourses/quotation/Save.java
@@ -0,0 +1,9 @@
+package view.recourses.quotation;
+
+import interfaces.View;
+
+public class Save implements View {
+    public void display(Object... params) {
+        System.out.println("\n\t\t--- Saving Quotation ---\n");
+    }
+}
