a37c080fa5965d496dfbb63cd55757ef21756e9d
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-24 19:40:10 +0100

    Update Entry.java

--- src/view/entry/Entry.java
+++ src/view/entry/Entry.java
@@ -11,7 +11,7 @@ public class Entry implements View {
         System.out.println("\t\t-Principal Menu-\t<0>\n");
         System.out.println("\t1. Create new project.");
         System.out.println("\t2. View all projects.");
-        System.out.println("\t3. Calculate the coute of a project.");
+        System.out.println("\t3. Calculate the cost of a project.");
         System.out.println("\t4. Quite.");
         Router.get(input.getNum("Enter your choice")).display();
     }
