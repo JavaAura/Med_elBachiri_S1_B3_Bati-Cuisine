6ba86db32307f0accdf7b1dd3fd583aafc7331d3
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-24 19:47:30 +0100

    Update Quotation model

--- src/model/Quotation.java
+++ src/model/Quotation.java
@@ -1,22 +1,31 @@
 package model;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 public class Quotation {
+    private LocalDate issueDate;
+    private LocalDate validUntil;
     private double estimatedAmount;
-    private Date issueDate;
-    private Date validUntil;
     private boolean accepted;
+    private int projectId;
 
-    public Quotation(double estimatedAmount, Date issueDate, Date validUntil, boolean accepted){
-        this.estimatedAmount = estimatedAmount;
+    public Quotation(LocalDate issueDate, LocalDate validUntil, double estimatedAmount){
         this.issueDate = issueDate;
         this.validUntil = validUntil;
-        this.accepted = accepted;
+        this.estimatedAmount = estimatedAmount;
     }
 
+    // getters
     public double getEstimatedAmount() { return estimatedAmount; }
-    public Date getIssueDate() { return issueDate; }
-    public Date getValidUntil() { return validUntil; }
+    public LocalDate getIssueDate() { return issueDate; }
+    public LocalDate getValidUntil() { return validUntil; }
     public boolean getIsAccepted(){ return this.accepted; }
+    public int getProjectId() { return projectId; }
+
+    // setters
+    public void setIssueDate(LocalDate issueDate) { this.issueDate = issueDate; }
+    public void setValidUntil(LocalDate validUntil) { this.validUntil = validUntil; }
+    public void setEstimatedAmount(double estimatedAmount) { this.estimatedAmount = estimatedAmount; }
+    public void setAccepted(boolean accepted) { this.accepted = accepted; }
+    public void setProjectId(int projectId) { this.projectId = projectId; }
 }
