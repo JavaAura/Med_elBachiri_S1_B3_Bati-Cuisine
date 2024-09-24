104b5ec84ad06fd40250dab73c56861a9444f7a2
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-24 22:13:44 +0100

    udpate get quotations n projects handler

--- src/daoImpl/QuotationDaoImpl.java
+++ src/daoImpl/QuotationDaoImpl.java
@@ -23,25 +23,35 @@ public class QuotationDaoImpl implements QuotationDao {
 
     @Override
     public boolean getAllQuotationsProjects() {
-        List<Quotation> quotations = new ArrayList<>();
-        List<Project> projects = new ArrayList<>();
         model.setQuery(
-                "SELECT p.id, p.name, p.benefit_margin, p.status, p.kitchen_area_m2, "+
+                "SELECT p.id, p.name, p.benefit_margin, p.status, p.kitchen_area_m2, " +
                         "q.id, q.estimated_amount, q.issue_date, q.valid_until, q.accepted " +
                         "FROM quotations q " +
-                        "JOIN projects p ON q.project_id = p.id "
+                        "RIGHT JOIN projects p ON q.project_id = p.id"
         );
-
         HashMap<Integer, List<Object>> data = model.getAll();
+
         data.forEach((k, v) -> {
             System.out.println(
-                    "\n\tPROJECT ID: " + v.get(0) + " | Name: " + v.get(1) + " | Benefit Margin: " +v.get(2) + " | Status: " + v.get(3) + " | Kitchen Surface: " + v.get(4)+"\n" +
-                    "\tQUOTATIONS ID: " + v.get(5) + " | Estimated Amount: " + v.get(6) + " | Issue Date: " + v.get(7) + " | Valid Until: " + v.get(8) + " | Accepted: " + ((boolean) v.get(9)? "YES" : "NO")
+                    "\n\tPROJECT ID: " + v.get(0) + " | Name: " + v.get(1) +
+                            " | Benefit Margin: " + v.get(2) + " | Status: " + v.get(3) +
+                            " | Kitchen Surface: " + v.get(4)
             );
+            if (v.get(5) != null) {
+                System.out.println(
+                        "\tQUOTATION ID: " + v.get(5) + " | Estimated Amount: " + v.get(6) +
+                                " | Issue Date: " + v.get(7) + " | Valid Until: " + v.get(8) +
+                                " | Accepted: " + (((boolean) v.get(9)) ? "YES" : "NO")
+                );
+            } else {
+                System.out.println("\tNo Quotation available for this project.");
+            }
         });
+
         System.out.println("\n\n");
         return data.isEmpty();
     }
+
 
     public boolean accept(int quotationId){
         model.activateWhere(true);
