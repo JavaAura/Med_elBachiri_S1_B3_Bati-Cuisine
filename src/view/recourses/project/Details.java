95bc91f2be722bb807b5cced1ded7cf504945b60
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-24 19:42:25 +0100

    Update project/Details.java

--- src/view/recourses/project/Details.java
+++ src/view/recourses/project/Details.java
@@ -1,30 +1,39 @@
 package view.recourses.project;
 
-
 import interfaces.View;
-import service.ClientService;
 import service.ProjectService;
+import view.routing.Router;
 
+import java.util.HashMap;
 
-public class Details implements View{
+public class Details implements View {
     private ProjectService service = new ProjectService();
+    private Double benefitMargin;
+    private Double tva;
 
-    public void display(Object... params){
-        int projectId = (int) params[0]; // project id
+    public void display(Object... params) {
+        int projectId = (int) params[0];
+        boolean saveQuotation = (boolean) params[1];
 
-        // get project and client, method return benefit margin
-        double benefitMargin = service.getProjectClient(projectId);
+        benefitMargin = service.getProjectClient(projectId);
+
+        double materialsCost = service.getMaterials(projectId);
+        double laborsCost = service.getLabors(projectId);
+        double totalCostBeforeMargin = materialsCost + laborsCost;
 
         System.out.println("\n\t\t--- Cost Breakdown ---\n");
-        double materialsCostTVA = service.getMaterials(projectId);
-        double laborsCostTVA = service.getLabors(projectId);
-        double totalCostBeforeMargin = materialsCostTVA + laborsCostTVA;
-        double benefitMarginCost = totalCostBeforeMargin * (benefitMargin/100);
+        System.out.printf("\n\t\t<3> Total cost without margin: \n\t\t%.2f MAD", totalCostBeforeMargin);
 
-        System.out.println("\n\t\t<3> Total cost before margin: \n\t\t" + totalCostBeforeMargin + "MAD");
-        System.out.println("\n\t\t<4> Profit margin ("+benefitMargin+"%): \n\t\t" + benefitMarginCost + "MAD");
-        System.out.println("\n\t\t**Final total cost of the project: \n\t\t" + (totalCostBeforeMargin + benefitMarginCost) + "MAD**");
+        double benefitMarginCost = (benefitMargin != null) ? totalCostBeforeMargin * (benefitMargin / 100) : 0;
+        double projectTotalCost = totalCostBeforeMargin + benefitMarginCost;
 
+        if (benefitMargin != null) {
+            System.out.printf("\n\t\t<4> Profit margin (%.2f%%): \n\t\t%.2f MAD", benefitMargin, benefitMarginCost);
+        }
 
+        System.out.printf("\n\t\t**Final total cost of the project: \n\t\t%.2f MAD**", projectTotalCost);
+        if (saveQuotation){
+            Router.get("project_save_quotation").display(projectId, projectTotalCost);
+        } else Router.get("entry").display();
     }
 }
