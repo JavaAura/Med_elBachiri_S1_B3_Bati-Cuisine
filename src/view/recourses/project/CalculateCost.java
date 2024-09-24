57d018135897da6c9c76b022088f6f240ec7306f
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-24 22:10:29 +0100

    Update CalculateCost.java

--- src/view/recourses/project/CalculateCost.java
+++ src/view/recourses/project/CalculateCost.java
@@ -1,35 +1,15 @@
 package view.recourses.project;
 
 import interfaces.View;
-import service.MaterialService;
-import service.ProjectService;
 import utils.Input;
 import view.routing.Router;
 
-public class CalculateCost implements View{
+public class CalculateCost implements View {
     private Input input = new Input();
-    private ProjectService service = new ProjectService();
 
-    private Double tva = null;
-    private Double benefitMargin = null;
-
-    public void display(Object... params){
-        int projectId = (Integer) params[0];
-
-        System.out.println("\t\t-Calculate Project Cost-\n\n");
-        if(input.getYesNo("Do you want to apply TVA to the project")) {
-            tva = input.getDouble("Enter the TVA percentage (%)");
-            input.cleanBuffer();
-            service.setTva(projectId, tva);
-        }
-        if(input.getYesNo("Do you want to apply a profit margin to the project")){
-            benefitMargin = input.getDouble("Enter the profit margin percentage (%) ");
-            input.cleanBuffer();
-            service.setBenefitMargin(projectId, benefitMargin);
-        }
-
-        Router.get("project_details").display(projectId, true);
+    public void display(Object... params) {
+        System.out.println("\t\t-Calculate the Cost of a Certain Project");
+        int projectId = input.getNum("Enter project ID");
+        Router.get("project_details").display(projectId, false);
     }
-
-
 }
