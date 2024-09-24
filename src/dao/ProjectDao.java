2c5bb9e5d24791e0517930e495008f3ca6525da6
Mohammed El Bachiri <144786147+mohammed-el-bachiri@users.noreply.github.com>
2024-09-24 19:48:01 +0100

    Update ProjectDao.java

--- src/dao/ProjectDao.java
+++ src/dao/ProjectDao.java
@@ -9,12 +9,14 @@ import java.util.HashMap;
 import java.util.List;
 
 public interface ProjectDao {
-    public HashMap<String, Project> getAll();
-    public Project get(int id);
-    public Integer create(Project project);
-    public List<Material> getMaterials(int projectId);
-    public List<Labor> getLabors(int projectId);
-    public boolean setTva(int projectId, double tva);
-    public boolean setBenefitMargin(int projectId, double benefitMargin);
-    public HashMap<String, Object> projectNClient(int projectId);
+    HashMap<String, Project> getAll();
+    Project get(int id);
+    Integer create(Project project);
+    List<Material> getMaterials(int projectId);
+    List<Labor> getLabors(int projectId);
+    boolean setTva(int projectId, double tva);
+    boolean setBenefitMargin(int projectId, double benefitMargin);
+    HashMap<String, Object> projectNClient(int projectId);
+    boolean delete(int projectId);
+    boolean update(Project project);
 }
